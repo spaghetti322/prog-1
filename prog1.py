561b90c44d98fa23a28bb7220e2654d1d5d28725
spaghetti322 <71093171+spaghetti322@users.noreply.github.com>
2021-09-13 22:41:38 +0300

    Update prog1.py
    4 и доделать 5

--- prog1.py
+++ prog1.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def is_even(number):
     if number < 0:
         return 'Error.'
@@ -17,15 +18,35 @@ def generate_squares(min_num, max_num):
 
 
 def split_list(unsorted_list):
-    i=0
-    while(i<(len(unsorted_list))):
+    i = 0
+    while(i < (len(unsorted_list))):
         if unsorted_list[i] == 0:
             unsorted_list.remove(0)
-        i+=1
+        i += 1
     return unsorted_list
 
 
-print(is_even(3))
-print(generate_squares(1, 5))
+def make_dict(some_list):
+    d = dict.fromkeys(['str', 'num'])
+
+
+class Vector2D:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def norm(self):
+        return np.sqrt((self.x)**2+(self.y)**2)
+
+    def __str__(self):
+        return f"x coordinate: {self.x}\ny coordinate: {self.y}\nnorm: {self.norm()}"
+
+    def __lt__(self, other):
+        return
+
 l = [1, 2, 0, 3, 0, 5, 6, 7]
-print(split_list(l))
+vector = Vector2D(1, 2)
+print(str(vector))
+'''print(is_even(4))
+print(generate_squares(1, 5))
+print(split_list(l))'''

d173b189affa1071c22e40a1f6435dd4d770673b
spaghetti322 <71093171+spaghetti322@users.noreply.github.com>
2021-09-17 21:41:13 +0300

    Update prog1.py
    ✔

--- prog1.py
+++ prog1.py
@@ -27,7 +27,21 @@ def split_list(unsorted_list):
 
 
 def make_dict(some_list):
-    d = dict.fromkeys(['str', 'num'])
+    #some_dict = dict.fromkeys(['str', 'num'])
+    l_num = []
+    l_str = []
+    l_str_counter = 0
+    l_num_counter = 0
+    for i in range(len(some_list)):
+        if isinstance(some_list[i], int) or isinstance(some_list[i], float):
+            l_num.append(some_list[i])
+            l_num_counter += 1
+        if isinstance(some_list[i], str):
+            l_str.append(some_list[i])
+            l_str.append(len(some_list[i]))
+            l_str_counter += 2
+    some_dict = {'str': l_str, 'num': l_num}
+    return some_dict
 
 
 class Vector2D:
@@ -42,11 +56,29 @@ class Vector2D:
         return f"x coordinate: {self.x}\ny coordinate: {self.y}\nnorm: {self.norm()}"
 
     def __lt__(self, other):
-        return
+        return (Vector2D.norm(self)) < (Vector2D.norm(other))
 
-l = [1, 2, 0, 3, 0, 5, 6, 7]
-vector = Vector2D(1, 2)
-print(str(vector))
-'''print(is_even(4))
-print(generate_squares(1, 5))
-print(split_list(l))'''
+    def __le__(self, other):
+        return (Vector2D.norm(self)) <= (Vector2D.norm(other))
+
+    def __eq__(self, other):
+        return self is other
+
+    def __ne__(self, other):
+        return self is not other
+
+    def __gt__(self, other):
+        return (Vector2D.norm(self)) > (Vector2D.norm(other))
+
+    def __ge__(self, other):
+        return (Vector2D.norm(self)) >= (Vector2D.norm(other))
+
+
+l = [1, 2, 'sda', 3., 0, '123h', 6, 7]
+vector=Vector2D(4,5.5)
+vector1=Vector2D(4,5.5)
+vector2=Vector2D(1,1)
+
+print(vector.norm())
+print(vector1.norm())
+print(vector>vector2)

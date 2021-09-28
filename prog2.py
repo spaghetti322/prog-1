0702ce09a0349f028ff9b53a97c0506479b2a191
spaghetti322 <71093171+spaghetti322@users.noreply.github.com>
2021-09-29 00:01:37 +0300

    done
    ✔

--- prog2.py
+++ prog2.py
@@ -1,15 +1,27 @@
 import datetime as DT
+import re
+import requests
 
-with open('F:/GitHub/prog-1/202-Advanced-Python-1/Practicum 1/log.txt') as file:
-    my_lines = list(file)
+url = 'https://raw.githubusercontent.com/vongostev/202-Advanced-Python-1/main/Practicum%201/log.txt'
+response = requests.get(url)
+my_lines = list(response.text.splitlines())
+
+# print(response.text)
+# with open('F:/GitHub/prog-1/202-Advanced-Python-1/Practicum 1/log.txt') as file:
+#     my_lines = list(file)
+
 useless_lines_counter = 0
 line = []
 modules = []
 DIWE_counter = []
 fun_counter = 0
 DIWE = ['[DEBUG', '[INFO', '[WARNING]', '[ERROR']
+modes_l = []
 for i in range(len(my_lines)):
     line.append(my_lines[i].split())
+    if re.search(r'Found \d{1,10} modes', my_lines[i]):
+        modes_l.append(
+            re.search(r'Found \d{1,10} modes', my_lines[i])[0].split()[1])
     # skipping useless lines:
     try:
         date = DT.datetime.strptime(str(line[i][0]), '%Y-%m-%d').date()
@@ -31,7 +43,6 @@ for i in range(len(my_lines)):
     #
     if 'fun:' in my_lines[i]:
         fun_counter += 1
-        
 dictionary_list = []
 for i in range(len(modules)):
     dictionary_list.append({modules[i]: {'DEBUG': DIWE_counter[i][1], 'INFO': DIWE_counter[i][3],
@@ -40,5 +51,6 @@ dictionary = {}
 
 for i in range(len(modules)):
     dictionary = {**dictionary, **dictionary_list[i]}
-print('Странный словарь:',dictionary)
-print('Число fun-ов:',fun_counter)
+print('Странный словарь:', dictionary)
+print('\nЧисло fun-ов:', fun_counter)
+print('\nСписок *Found x modes*:', modes_l)

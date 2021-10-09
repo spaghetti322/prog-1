2bba96c0bc79ceaf683ab24c99f10de8b3095d38
spaghetti322 <71093171+spaghetti322@users.noreply.github.com>
2021-10-09 22:11:43 +0300

    ssssss
    1,2,3 rdy

--- prog3 numpy.py
+++ prog3 numpy.py
@@ -6,39 +6,42 @@ import matplotlib.pyplot as plt
 def poisson(lambda_, N):
     if lambda_ < 0:
         raise IOError("Лямбда меньше 0")
-    n = np.arange(0, N+1)
+    n = np.arange(0, N + 1)
     e = np.exp(1)
-    return (lambda_**n)*(e**(-lambda_))/factorial(n)
+    p = (lambda_ ** n) * (e ** (-lambda_)) / factorial(n)
+    return np.array([n, p])
 
 
 def moment(arr, k):
-    if not isinstance(arr, np.ndarray) or not isinstance(k, int):
-        raise IOError("Неправильные значения на вход")
-    m = arr[0:k]
-    m_index = np.arange(0, k)
-    return np.sum(m*m_index)
+    if not isinstance(arr, np.ndarray) or not isinstance(k, int) or k < 0:
+        raise IOError("Неправильные значения на вход для момента")
+    return np.sum(arr[0] ** k * arr[1])
 
 
 def expected_value(arr):
     if not isinstance(arr, np.ndarray):
-        raise IOError("Неправильные значения на вход")
-    return moment(arr, (arr.size))
+        raise IOError("Неправильные значения на вход для мат ожидания")
+    return moment(arr, 1)
 
 
 def dispersion(arr):
     if not isinstance(arr, np.ndarray):
-        raise IOError("Неправильные значения на вход")
-    mid = expected_value(arr)
-    ff = (arr-mid)**2
-    f = expected_value(ff)
-    return f
+        raise IOError("Неправильные значения на вход для дисперсии")
+    arr[0] = (arr[0] - expected_value(arr)) ** 2
+    return expected_value(arr)
 
 
-test_k = 111
-test_p = poisson(3, test_k)
-# print(test_p)
-print(moment(test_p, 2))
-print(expected_value(test_p))
-print(dispersion(test_p))
-plt.plot(test_p)
-plt.show()
+if __name__ == '__main__':
+    lambda_ = 3
+    N = 333
+    k = 2
+    p = poisson(lambda_, N)
+    average_expected_value_fan = expected_value(p)
+    average_dispersion_enjoyer = dispersion(p)
+    # print(moment(p, k))
+    # print(average_expected_value_fan)
+    # print(average_dispersion_enjoyer)
+    assert np.allclose([average_expected_value_fan,
+                        average_dispersion_enjoyer], [lambda_, lambda_], 1e-3)
+    plt.plot(p[1])
+    plt.xlim(-1,50)
